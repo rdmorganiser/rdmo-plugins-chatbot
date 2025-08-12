537de92f07fa91ce1644abd2d465a0fbce9a1f63
Jochen Klar <mail@jochenklar.de>
2025-08-12 17:44:57 +0200

    Use setuptools version_file

--- rdmo_chatbot/__init__.py
+++ rdmo_chatbot/__init__.py
@@ -1,1 +1,4 @@
-__version__ = "1.0.0"
+try:
+    from ._version import __version__, __version_tuple__, version, version_tuple
+except ImportError:
+    __version__ = __version_tuple__ = version = version_tuple = None
